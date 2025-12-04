e700b3677d0d045802611e3df4ee811d474336dd
Rubal-code <rubal3232@gmail.com>
2025-12-04 22:24:46 +0530

    assignment 3 done

---
+++ problems_in_java/assignment_3/next_multiple_of_five.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+    // Write your code here
+    List<Integer>result=new ArrayList<>();
+    for(int grade:grades){
+        if(grade<38){
+            result.add(grade);
+        }
+        else{
+            int next_multiple_of_five=((grade/5)+1)*5;
+            //for prev =(grade/5)*5;
+            if(next_multiple_of_five-grade<3){
+                result.add(next_multiple_of_five);
+            }
+            else{
+                result.add(grade);
+            }
+        }
+        
+    }
+    return result;
+    }
+
+}
+
+public class next_multiple_of_five {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = new ArrayList<>();
+
+        for (int i = 0; i < gradesCount; i++) {
+            int gradesItem = Integer.parseInt(bufferedReader.readLine().trim());
+            grades.add(gradesItem);
+        }
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        for (int i = 0; i < result.size(); i++) {
+            bufferedWriter.write(String.valueOf(result.get(i)));
+
+            if (i != result.size() - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
