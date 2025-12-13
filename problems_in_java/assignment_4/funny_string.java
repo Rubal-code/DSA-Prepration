713e19f1cc249a21ce9e7432ec1f66ccacd9ea9f
Rubal-code <rubal3232@gmail.com>
2025-12-13 21:14:25 +0530

    assignment4 added

---
+++ problems_in_java/assignment_4/funny_string.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'funnyString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String funnyString(String s) {
+    // Write your code here
+    int n=s.length();
+    for (int i=1;i<n;i++){
+        int diff1=Math.abs(s.charAt(i)-s.charAt(i-1));
+        int diff2=Math.abs(s.charAt(n-i)-s.charAt(n-1-i));
+        
+        if(diff1!=diff2){
+        return "Not Funny";
+    }
+    }
+    return "Funny";
+    }
+
+}
+
+public class funny_string{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = bufferedReader.readLine();
+
+            String result = Result.funnyString(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ problems_in_java/assignment_4/hackerrank_in_a_string.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'hackerrankInString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String hackerrankInString(String s) {
+    // Write your code here
+        String result="hackerrank";
+        int j=0;
+        for (int i=0;i<s.length()&&j<result.length();i++){
+            if(s.charAt(i)==result.charAt(j)){
+            j++;
+            }
+        }
+        if(j==result.length()){
+            return "YES";
+        }
+        else{
+            return "NO";
+        }
+    }
+
+}
+
+public class hackerrank_in_a_string{
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = bufferedReader.readLine();
+
+            String result = Result.hackerrankInString(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
